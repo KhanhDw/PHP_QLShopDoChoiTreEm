73380b76d2166486059fa279902d06f3a2eadc56
TUONGVI244 <vi_dth216244@student.agu.edu.vn>
2024-12-17 19:20:59 +0700

    Bổ sung hoàn chỉnh Index trong public - VI

--- public/index.php
+++ public/index.php
@@ -93,5 +93,146 @@ switch($action)
             $dochoi = $dc->laydochoi();	   
             include("cart.php");
             break;
+        
+         case "doisoluong":
+                $ghang = new GIOHANG(); 
+                if(isset($_GET["idgh"]))
+                    $ghang->setid($_GET["idgh"]);
+                if(isset($_GET["soluong"])){
+                    if($_GET["soluong"]==0){
+                        $gh->xoagiohang($_GET["idgh"]);
+                        $giohang = $gh->laygiohangtheoidnguoidung($_SESSION["nguoidung"]["id"]);   
+                        $dochoi = $dc->laydochoi();	   
+                        include("cart.php");
+                        break;
+                    }else {
+                        $ghang->setsoluong($_GET["soluong"]);
+                    }
+                }
+                $ghang->setnguoidung_id($_SESSION["nguoidung"]["id"]);
+                $gh->capnhatsoluong($ghang);
+                $giohang = $gh->laygiohangtheoidnguoidung($_SESSION["nguoidung"]["id"]); 
+                $dochoi = $dc->laydochoi();	     
+                include("cart.php");
+                break;
+         case "xoagiohang":
+                if(isset($_GET["id"]))
+                    $gh->xoagiohang($_GET["id"]);
+                $giohang = $gh->laygiohangtheoidnguoidung($_SESSION["nguoidung"]["id"]);   
+                $dochoi = $dc->laydochoi();	   
+                include("cart.php");
+                break;
+            case "lapdon":        
+                $giohang = $gh->laygiohangtheoidnguoidung($_SESSION["nguoidung"]["id"]);   
+                $dochoi = $dc->laydochoi();
+                $giamgia = $gg->laygiamgia();
+            // $h = $dh->laydonhangtheoid($_GET["id"]);
+                $chons = $_POST['ghchon']; 
+                if (!empty($chons)) {
+                    $tongtien = 0;
+                    $tiengiam = 0;
+                    $thanhtien = 0;
+                    foreach($giohang as $g)
+                        foreach($dochoi as $sh)
+                            foreach($chons as $chon)
+                                if($g["dochoi_id"]==$sh["id"] && $g["id"]==$chon) {
+                                    $tongtien += $sh["giagoc"]*$g["soluong"];
+                                    $tiengiam += $sh["giagoc"]*$g["soluong"]*$sh["giam"]/100;
+                                }
+                    $thanhtien = $tongtien - $tiengiam;
+                    include("order.php");
+                }
+                // $combo = $cb->laycombo();
+                $giohang = $gh->laygiohangtheoidnguoidung($_SESSION["nguoidung"]["id"]);   
+                $dochoi = $dc->laydochoi();	   
+                include("cart.php");
+                break;
+        case "thanhtoan":
+                if (!empty($_SESSION["numbers"])) {
+                    $chons = $_SESSION["numbers"];
+                    $dhang = new DONHANG();
+                    $giohang = $gh->laygiohangtheoidnguoidung($_SESSION["nguoidung"]["id"]); 
+                    $dochoi = $dc->laydochoi();
+                    $tongtien = 0;
+                    $tiengiam = 0;
+                    $thanhtien = 0;
+                    foreach($giohang as $g)
+                        foreach($dochoi as $sh)
+                            foreach($chons as $chon)
+                                if($g["dochoi_id"]==$sh["id"] && $g["id"]==$chon) {
+                                    $tongtien += $sh["giagoc"]*$g["soluong"];
+                                    $tiengiam += $sh["giagoc"]*$g["soluong"]*$sh["giam"]/100;
+                                }
+                    $thanhtien = $tongtien - $tiengiam;
+                    $dhang->setnguoidung_id($_SESSION["nguoidung"]["id"]);
+                    $dhang->settongtien($tongtien);
+                    $dhang->settiengiam($tiengiam);
+                    $dhang->setthanhtien($thanhtien);
+                    $dhang->setdiachi($_POST["txtdiachi"]);
+                    $dh->themdonhang($dhang);
+                    $dhang = $dh->laydonhangmoinhat();
+                    foreach($giohang as $g)
+                        foreach($dochoi as $sh)
+                            foreach($chons as $chon)
+                                if($g["dochoi_id"]==$sh["id"] && $g["id"]==$chon) {
+                                    $ct = new CHITIETDONHANG();
+                                    $ct->setdonhang_id($dhang["id"]);
+                                    $ct->setdochoi_id($sh["id"]);
+                                    $ct->setdongia($sh["giagoc"]-$sh["giagoc"]*$sh["giam"]/100);
+                                    $ct->setsoluong($g["soluong"]);
+                                    $ct->setthanhtien($ct->getdongia()*$ct->getsoluong());
+                                    $ctdh->themctdonhang($ct);
+                                    $gh->xoagiohang($g["id"]);
+                                }
+                    $_SESSION["numbers"] = []; 
+                    
+                    include("message.php");
+                    break;
+                }
+                $dochoi = $dc->laydochoi();	
+                include("main.php");
+                break;
+        case "dangnhap":
+                include("loginform.php");
+                break;
+        case "xldangnhap":
+                $email = $_POST["txtemail"];
+                $matkhau = $_POST["txtmatkhau"];
+                $nd = new NGUOIDUNG();
+                if($nd->kiemtranguoidunghople($email,$matkhau)==TRUE){
+                    $_SESSION["nguoidung"] = $nd->laythongtinnguoidung($email);
+                    // đọc thông tin (đơn hàng) của kh
+                    $donhang = $dh->laydonhangtheoidnguoidung($_SESSION["nguoidung"]["id"]);
+                    $ctdonhang = $ctdh->layctdonhang();
+                    $dochoi = $dc->laydochoi();
+                    include("info.php");
+                }
+                else{
+                    //$tb = "Đăng nhập không thành công!";
+                    include("loginform.php");
+                }
+                break;
+        case "thongtin":
+                // đọc thông tin các đơn của khách
+                    // đọc thông tin (đơn hàng) của kh
+                $donhang = $dh->laydonhangtheoidnguoidung($_SESSION["nguoidung"]["id"]);
+                $ctdonhang = $ctdh->layctdonhang();
+                $dochoi = $dc->laydochoi();
+                include("info.php"); // trang info.php hiển thị các đơn đã đặt
+                break;
+        case "dangxuat":
+                unset($_SESSION["nguoidung"]);
+                // chuyển về trang chủ
+                $dochoi = $dc->laydochoi();   
+                include("main.php");
+                break;
+            default:
+                break;
+    
+        case "danhgia":
+            $danhgia = $dg->laydanhgia($_SESSION["dochoi"]["id"]);
+            $dochoi = $_POST['dochoi_id'];
+            $nguoidung = $_SESSION['nguoidung_id'];
+                    
         }
 ?>
