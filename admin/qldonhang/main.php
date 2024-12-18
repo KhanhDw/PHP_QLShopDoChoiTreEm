451ef5a12347e6b2c5dcac7cde15267d95380976
LeviVN100 <LeviVN100@gmail.com>
2024-12-18 18:08:16 +0700

    Khai up main qldonhang

---
+++ admin/qldonhang/main.php
@@ -0,0 +1,29 @@
+<?php include("../inc/top.php"); ?>
+
+<h3>Quản lý đơn hàng</h3> 
+<br>
+<br>
+<table class="table table-hover">
+	<tr>
+        <th>ID</th>
+		<th>ID khách hàng</th>
+		<th>Tổng tiền</th>
+        <th>Tiền giảm</th>
+        <th>Thành tiền</th>
+	</tr>
+	<?php
+	foreach($donhang as $dh):
+	?>
+	<tr>
+        <td><a href="index.php?action=chitiet&id=<?php echo $dh["id"]; ?>"><?php echo $dh["id"]; ?></a></td>
+        <td><?php echo $dh["nguoidung_id"]; ?></td>
+        <td><?php echo number_format($dh["tongtien"]); ?> VND</td>
+        <td><?php echo number_format($dh["tiengiam"]); ?> VND</td>
+		<td><?php echo number_format($dh["thanhtien"]); ?> VND</td>	
+	</tr>
+	<?php
+	endforeach;
+	?>
+</table>
+
+<?php include("../inc/bottom.php"); ?>
